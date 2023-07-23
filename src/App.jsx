18c34a6273f434f7669e1232d8a7d9667f858cba
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-07-24 01:42:26 +0530

    News app classbased
    News app made with class bassed components

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import News from './components/News'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+export class App extends Component {
+pageSize = 6
+  render() {
+    return (
+      <BrowserRouter>
+    <Navbar/>
+
+    {/* <News pageSize={this.pageSize} country='in' category='business' /> */}
+    <Routes>
+      <Route exact path='/' element={<News key='general' pageSize={this.pageSize} country='in' category='general' />} />
+      <Route exact path='/business' element={<News key='business' pageSize={this.pageSize} country='in' category='business' />} />
+      <Route exact path='/entertainment' element={<News key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' />} />
+      <Route exact path='/health' element={<News key='health' pageSize={this.pageSize} country='in' category='health' />} />
+      <Route exact path='/science' element={<News key='science' pageSize={this.pageSize} country='in' category='science' />} />
+      <Route exact path='/sports' element={<News key='sports' pageSize={this.pageSize} country='in' category='sports' />} />
+      <Route exact path='/technology' element={<News key='technology' pageSize={this.pageSize} country='in' category='technology' />} />
+
+    </Routes>
+
+    </BrowserRouter>
+    )
+  }
+}
+
+export default App
---
+++ src/components/News.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './spinner'
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export class News extends Component {
+  articles = []
+  static defaltProps = {
+    country: 'in',
+    pageSize: 4,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+  cap = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: this.articles,
+      loading: false,
+      page: 1,
+      totalResults: 0
+    }
+
+    document.title = `${this.cap(this.props.category)} - NewsInternational`;
+  }
+
+  async updateNews() {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=6e30e3062cab4f3fbbec24c4aca28839&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({ loading: true })
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    // console.log(parsedData, 1)
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
+    })
+  }
+
+  componentDidMount() {
+    this.updateNews();
+  }
+
+  fetchMoreData = async () => {
+    // this.setState({ page: this.state.page + 1, })
+    // console.log(this.state.page)
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=6e30e3062cab4f3fbbec24c4aca28839&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData, this.state.page)
+    this.setState({
+      page: this.state.page + 1,
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+    })
+  };
+
+  render() {
+    return (
+      <>
+        <h2 style={{ margin: '40px' }}>NewInternational - Top {this.cap(this.props.category)} Headlines </h2>
+        {this.state.loading && <Spinner />}
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+          // endMessage={<p>Ends Here</p>}
+          >
+          <div className="container">
+            <div className="row d-flex flex-wrap justify-content-center">
+              {this.state.articles.map((element) => {
+                return <div className="col md-4" key={element.url}>
+                  <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage ? element.urlToImage : "https://www.gannett-cdn.com/presto/2023/06/28/USAT/1ae86d68-7784-451b-b6a2-aa8558ba0e97-AP23178343313246.jpg?auto=webp&crop=2080,1170,x0,y372&format=pjpg&width=1200"} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                </div>
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    )
+  }
+}
+
+export default News
